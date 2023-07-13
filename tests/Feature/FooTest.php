c92f867ba1e89434ce269abe33ed7be2e8a600f4
Bert Bijnens <bertbijnens@MacBook-Pro-van-Bert.local>
2023-07-13 16:10:22 +0200

    Added openapi docs verification

--- tests/Feature/FooTest.php
+++ tests/Feature/FooTest.php
@@ -2,6 +2,10 @@
 
 use App\Models\Foo;
 
+function foo() {
+    return Foo::factory()->create();
+}
+
 it('should return 302 when not authenticated: /api/foos', function () {
     $response = $this->get('/api/foos');
 
@@ -24,43 +28,15 @@ it('should return 200 when authenticated: /api/foos (json)', function () {
         ->toBeArray();
 });
 
-it('should return user when authenticated: /api/foos (json)', function () {
-    $foo = Foo::factory()->create();
+it('should return 1 foo: /api/foos (json)', function () {
+    foo();
 
     $response = $this->actingAs(user(), 'api')
-        ->json('GET', '/api/foos/'.$foo->id);
+        ->json('GET', '/api/foos');
 
     $response->assertStatus(200);
 
     expect($response->getData())
-        ->toBeObject()
-        ->id->toBe($foo->id)
-        ->name->toBe($foo->name);
-});
-
-it('should create new user', function () {
-    $data = Foo::factory()->make();
-    $response = $this->actingAs(user(), 'api')
-        ->json('POST', '/api/foos', $data->toArray());
-
-    $response->assertStatus(200);
-
-    expect($response->getData())
-        ->toBeObject()
-        ->id->toBeUuid()
-        ->name->toBe($data->name);
-});
-
-it('should update current user', function () {
-    $foo = Foo::factory()->create();
-
-    $data = Foo::factory()->make();
-    $response = $this->actingAs(user(), 'api')
-        ->json('POST', '/api/foos/'.$foo->id, $data->toArray());
-
-    $response->assertStatus(200);
-
-    expect($response->getData())
-        ->toBeObject()
-        ->name->toBe($data->name);
+        ->toBeArray()
+        ->toHaveCount(1);
 });
---
+++ tests/Setup/ValidateDocs.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Tests\Setup;
+
+use Illuminate\Contracts\Http\Kernel as HttpKernel;
+use Illuminate\Foundation\Testing\Concerns\MakesHttpRequests;
+use Illuminate\Http\Request;
+use Osteel\OpenApi\Testing\ValidatorBuilder;
+use Osteel\OpenApi\Testing\ValidatorInterface;
+use Symfony\Component\HttpFoundation\Request as SymfonyRequest;
+
+trait ValidateDocs
+{
+    use MakesHttpRequests;
+
+    protected ValidatorInterface $validator;
+
+    public function getValidator(): ValidatorInterface {
+        if(!isset($this->validator) || !$this->validator) {
+            $this->validator = ValidatorBuilder::fromYamlFile(resource_path('docs/openapi.yaml'))->getValidator();
+        }
+
+        return $this->validator;
+    }
+
+    public function call($method, $uri, $parameters = [], $cookies = [], $files = [], $server = [], $content = null) {
+        $kernel = $this->app->make(HttpKernel::class);
+
+        $files = array_merge($files, $this->extractFilesFromDataArray($parameters));
+
+        $symfonyRequest = SymfonyRequest::create(
+            $this->prepareUrlForRequest($uri), $method, $parameters,
+            $cookies, $files, array_replace($this->serverVariables, $server), $content
+        );
+
+        $response = $kernel->handle(
+            $request = Request::createFromBase($symfonyRequest)
+        );
+
+        $kernel->terminate($request, $response);
+
+        $status_code = $response->getStatusCode();
+        if($status_code >= 200 && $status_code < 300) {
+            $validator = $this->getValidator();
+
+            $validator->validate($request, $uri, $method);
+            $validator->validate($response, $uri, $method);
+        }
+
+        if ($this->followRedirects) {
+            $response = $this->followRedirects($response);
+        }
+
+        return static::$latestResponse = $this->createTestResponse($response);
+    }
+}
