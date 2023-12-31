706693d661974e2af4dd0469ecd317f3c3724828
Bert Bijnens <bertbijnens@MacBook-Pro-van-Bert.local>
2023-12-31 17:33:23 +0100

    Added language middleware

---
+++ app/Http/Middleware/LanguageMiddleware.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class LanguageMiddleware
+{
+    public function handle(Request $request, Closure $next)
+    {
+        if ($request->hasHeader('Accept-Language')) {
+            $preferredLanguage = $request->getPreferredLanguage();
+            $preferredLanguage = $this->formatLanguage($preferredLanguage);
+
+            app()->setLocale(
+                $preferredLanguage
+            );
+        }
+
+        return $next($request);
+    }
+
+    public function formatLanguage(string $language): string {
+        $position = strpos($language, '_');
+
+        if(!$position) {
+            return $language;
+        }
+
+        return substr($language, 0, $position);
+    }
+}
